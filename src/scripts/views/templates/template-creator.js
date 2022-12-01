4e189e25a78abb720f62670cfd96892cdb800cbe
Astanti Hariani Pratiwi <astanti.hariani@gmail.com>
2022-12-02 01:34:27 +0700

    edit responsive

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -7,88 +7,87 @@
 import API_ENDPOINT from "../../globals/api-endpoint";
 
 const createRestaurantDetailTemplate = (restaurant) => `
-<div class="detail"
+<img class="restaurant__poster" src="${API_ENDPOINT.RESTAURANT_IMAGE + restaurant.pictureId}" alt="${restaurant.name}"/>
+<div class="restaurant__info">
   <h2 class="restaurant__title">${restaurant.name}</h2>
-  <img class="restaurant__poster" src="${API_ENDPOINT.RESTAURANT_IMAGE + restaurant.pictureId}" alt="${restaurant.name}"/>
+  <h4>address : <span>${restaurant.address}</span></h4>
+  <h4>City : <span>${restaurant.city}</span></h4>
+  <h4>Rating : <span>${restaurant.rating}</span></h4>
 </div>
 
-  <div class="restaurant__info">
-    <h3>Information</h3>
-    <h4>City</h4>
-    <p>${restaurant.city}</p>
+<h3 class="titlemenu">Information</h3>
 
-    <h4>Address</h4>
-    <p>${restaurant.address}</p>
+<div class="menu">
+
+    <div class="food">
+    <h4>Food</h4>
+    <ul>${restaurant.menus.foods
+    .map(
+      (food) => `
+      <li>${food.name}</li>
+      `,
+    )
+    .join('<br>')}</ul>
+    </div>
     
-    <h4>Rating</h4>
-    <p>${restaurant.rating}</p>
+    <div class="drink">
+    <h4>Drink</h4>
+    <ul>${restaurant.menus.drinks
+    .map(
+      (drink) => `
+    <li>${drink.name}</li>
+    `,
+    )
+  .join('<br>')}</ul>
+    </div>
+</div>
     
+<div class="restaurant__overview">
+
+  <div class="categories">
     <h4>Categories</h4>
-    <div>${restaurant.categories
+    <p>${restaurant.categories
       .map(
         (category) => `
            <p>${category.name}</p>
     `
       )
-  .join("")} </div>
-      
+  .join("")} <p>
   </div>
-
-  <div class="restaurant__overview">
-    <h3>Overview</h3>
+      
+ 
+  <div class="description">
+    <h4>Description</h4>
     <p>${restaurant.description}</p>
   </div>
 
-  <h3>Menu List</h3>
-  <div class="restaurant-detail__menu-list">
-          <div class="foods">
-              <h4>Foods</h4>
-              <ul class="restaurant-detail__foods">
-                  ${restaurant.menus.foods.map((food) => `
-      <li><i class="fa fa-cutlery font-decoration"></i> ${food.name}</li>
-    `).join('')}
-              </ul>
-          </div>
-
-          <div class="drinks">
-              <h4>Drinks</h4>
-              <ul class="restaurant-detail__drinks">
-                  ${restaurant.menus.drinks.map((drink) => `
-      <li><i class="fa fa-coffee font-decoration"></i> ${drink.name}</li>
-    `).join('')}
-              </ul>
-          </div>
-  
-  <h3>Review Customer</h3>
-  <div class="restaurant-detail__review">
-  ${restaurant.customerReviews.map((customer) => `
-        <div class="review-container">
-          <i class="fa fa-user-circle customer-avatar font-secondary"></i>
-          <div class="customer-name">
-            <h4>${customer.name}</h4>
-            <span class="customer-review-date">${customer.date}</span>
-          </div>
-          <p class="customer-review">${customer.review}</p>
-        </div>
-      `).join('')}
-            </div>
+  <div class="review">
+  <h4>Reviews</h4>
+  <p>${restaurant.customerReviews
+    .map(
+      (review) => `
+    <p>${review.name}</p>
+    <p>${review.review}</p>
+    <p>${review.date}</p>
+    `,
+    )
+    .join('<br>')}
+  </div>
+</div>
 `;
 
 const createRestaurantItemTemplate = (restaurant) => `
-  <div class="restaurant-item">
+  <div class="restaurant-item" tabindex="0">
     <div class="restaurant-item__header">
       <img class="restaurant-item__header__poster" alt="${restaurant.name}" title="${restaurant.name}"
            src="${API_ENDPOINT.RESTAURANT_IMAGE + restaurant.pictureId}">
-      <div class="city">${restaurant.city}</div>
-    </div>
-
-    <div class="restaurant-item__header__rating">
-    <p>Rating: <span class="restaurant-item__score">${restaurant.rating}</span></p>
     </div>
     
     <div class="restaurant-item__content">
-    <h2><a href='/#/detail/${restaurant.id}'>${restaurant.name}</a></h2>
-    <p class="restaurant-item_desc">${restaurant.description}</p>
+    <h2 class="post_title"><a href='/#/detail/${restaurant.id}'>${restaurant.name}</a></h2>
+    <p class="restaurant_city">City: ${restaurant.city}</p>
+    <p class="restaurant_city">Rating: ${restaurant.rating}</p>
+    <p class="restaurant_descripstion">${restaurant.description}</p>
     </div>
   </div>
 `;
